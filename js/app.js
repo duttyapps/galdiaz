a2c5e864e4dfa3f2d1483fc5431be895c01fa590
Desarrollador 3 <desarrollo3@cineplanet.com.pe>
2018-05-03 18:17:01 -0500

    ending first page

--- js/app.js
+++ js/app.js
@@ -6,9 +6,14 @@ $(function () {
     $("#btnShowMore").click(function () {
         x = (x + 4 <= products_size) ? x + 4 : products_size;
         $("#listProducts .products:lt(" + x + ")").show();
-        if(x == products_size) {
+        if (x == products_size) {
             $(this).hide();
         }
     });
-    console.log(products_size);
+
+    $('#btnShowLess').click(function () {
+        x = (x - 4 < 0) ? 4 : x - 4;
+        $('#listProducts .products').not(':lt(' + x + ')').hide();
+    });
+    
 });
